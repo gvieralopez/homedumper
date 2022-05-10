894264ee7dbea5d2cbe7ff0fce2c1137a7b5494e
Gustavo Viera López <gvieralopez@gmail.com>
2022-05-11 01:48:04 +0200

    Add dump command

--- homedumper/__main__.py
+++ homedumper/__main__.py
@@ -1,9 +1,42 @@
+import pathlib
 import typer
 import homedumper
 from homedumper.const import DEFAULT_OUT
 
 app = typer.Typer()
 
+
+@app.command()
+def dump(video_path: str, output_path: str = DEFAULT_OUT):
+    """
+    Dumps the database from the video.
+
+    Parameters
+    ----------
+    video_path : str
+        Path to the video to extract frames from.
+    output_path : str, optional
+        Path to the output folder, by default DEFAULT_OUT
+    """
+    # Exctract frames from the video
+    count = homedumper.extract(video_path=video_path, output_path=output_path)
+    typer.echo(f"Extracted {count} frames from {video_path}")
+
+    # Locate the project folder
+    video_path = pathlib.Path(video_path)
+    folder_path = pathlib.Path(output_path) / video_path.stem
+
+    # Boxify the frames
+    count = homedumper.boxify(folder_path=folder_path)
+    typer.echo(f"{count} frames converted to box from {folder_path}")
+
+    # Download resurces
+    homedumper.download()
+    typer.echo(f"All resources downloaded")
+
+    # Match all thumbnails to pokemon names
+    count = homedumper.match(path=folder_path)
+    typer.echo(f"{count} pokemon found in {folder_path}")
 
 @app.command()
 def extract(video_path: str, output_path: str = DEFAULT_OUT):
