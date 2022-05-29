559a50fdf8d0e8622f6b58cc2266ec743a4c7306
Gustavo Viera López <gvieralopez>
2022-05-29 12:08:26 +0200

    Improve evaluation script.

--- scripts/eval_extraction.py
+++ scripts/eval_extraction.py
@@ -3,74 +3,10 @@
 # the sample video.
 
 import json
-import csv
-
-
-def emptybox(name: str) -> dict:
-    """
-    Creates an empty box.
-
-    Parameters
-    ----------
-    name : str
-        Name of the box.
-
-    Returns
-    -------
-    dict
-        Dictionary with the empty box.
-    """
-
-    return {
-      "title": name,
-      "pokemon": [None for i in range(30)]
-    }
-
-def csv2json(csvreader: csv.reader) -> dict:
-    """
-    Converts a csv reader to a json list.
-
-    Parameters
-    ----------
-    csvreader : csv.reader
-        csv reader object to convert
-
-    Returns
-    -------
-    dict
-        Dictionary with the extracted data in json format.
-    """
-
-    extracted = list(csvreader)
-    data = {
-        "name": "Dumped",
-        "slug": "dumped",
-        "description": "Pokémon Boxes dumped from the video",
-        "boxes": []
-    }
-
-    current_box_name = None
-    current_box = None
-
-    for box_name, slot_id, pokemon_id in extracted[1:]:
-
-        if current_box is None:
-            current_box_name = box_name
-            current_box = emptybox(box_name)
-
-        if box_name != current_box_name:
-            current_box_name = box_name
-
-            data["boxes"].append(current_box)
-            current_box = emptybox(box_name)
-        
-        current_box["pokemon"][int(slot_id)-1] = pokemon_id
-
-    return data
 
 
 
-def compare_boxes(box1: dict, box2: dict) -> tuple[int, int, int]:
+def compare_boxes(box1: dict, box2: dict) -> tuple[int, int, int, int, list]:
     """
     Compares two boxes.
 
@@ -83,13 +19,16 @@ def compare_boxes(box1: dict, box2: dict) -> tuple[int, int, int]:
 
     Returns
     -------
-    tuple[int, int, int]
-        Number of correct, incorrect and form-incorrect pokemon.
+    tuple[int, int, int, int, list]
+        Number of correct, species-incorrect, form-incorrect and fake-deteted 
+        pokemon followed by a list of incorrect matches.
     """
 
+    errors = []
     correct = 0
-    incorrect = 0
+    species_incorrect = 0
     form_incorrect = 0
+    fake_detected = 0
 
     for i in range(30):
         pkm1 = box1["pokemon"][i] if i < len(box1["pokemon"]) else None
@@ -97,14 +36,23 @@ def compare_boxes(box1: dict, box2: dict) -> tuple[int, int, int]:
 
         if pkm1 is None and pkm2 is None:
             continue
+        if pkm1 is None and pkm2 is not None:
+            fake_detected += 1
+            errors.append((pkm1, pkm2, "fake detect"))
+            continue
         if pkm1 == pkm2:
             correct += 1
-        elif pkm1 and pkm2 and (pkm1.startswith(pkm2) or pkm2.startswith(pkm1)):
-            form_incorrect += 1
-        else:
-            incorrect += 1
+            continue
+        if pkm1 is not None and pkm2 is not None:
+            print(pkm1, pkm2)
+            if pkm1.split("-")[0] == pkm2.split("-")[0]:
+                form_incorrect += 1
+                errors.append((pkm1, pkm2, "form"))
+            continue
+        errors.append((pkm1, pkm2, "species"))
+        species_incorrect += 1
 
-    return correct, incorrect, form_incorrect
+    return correct, species_incorrect, form_incorrect, fake_detected, errors
 
 
 
@@ -113,27 +61,31 @@ if __name__ == "__main__":
     with open('data/perfect.json') as f:
         ground_truth = json.load(f)
 
-    try:
-        with open('../homedumper/output/myhome/match.csv', 'r') as f:
-            extracted = csv.reader(f)
-            extracted = csv2json(extracted)
-    except FileNotFoundError:
-        print('No match.csv found. Please run the matching script first.')
-        exit()
+    with open('../homedumper/output/myhome/match.json') as f:
+        predicted = json.load(f)
 
     
     correct = 0
-    incorrect = 0
+    species_incorrect = 0
     form_incorrect = 0
+    fake_detected = 0
+    errors = []
 
-    for gt_box, dp_box in zip(ground_truth["boxes"], extracted["boxes"]):
-        c, i, f = compare_boxes(gt_box, dp_box)
+    for gt_box, dp_box in zip(ground_truth["boxes"], predicted["boxes"]):
+        c, si, fi, vi, err = compare_boxes(gt_box, dp_box)
         correct += c
-        incorrect += i
-        form_incorrect += f
+        species_incorrect += si
+        form_incorrect += fi
+        fake_detected += vi
+        errors += err
 
-    
-    print(f'Correct: {correct}')
-    print(f'Incorrect: {incorrect}')
-    print(f'Form-incorrect: {form_incorrect}')
+    print('Bad matches: \n')
+    for er in errors:
+        print(f'[{er[2]}] {er[0]} -> {er[1]}')
 
+    print(f'\nCorrect: {correct}')
+    print(f'Incorrect: {species_incorrect + form_incorrect + fake_detected}')
+    print(f'     Species: {species_incorrect}')
+    print(f'     Form: {form_incorrect}')
+    print(f'     Empty slots: {fake_detected}')
+ 
